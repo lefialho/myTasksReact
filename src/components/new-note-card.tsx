ed34f2cce99d533522b4a2edd7d267da00a9702b
Leandro Fialho <leandroaparecido13@yahoo.com.br>
2025-05-23 19:43:42 -0300

    Ajuste no speechRecognition

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -66,7 +66,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
     speechRecognition.lang = 'pt-BR';
     speechRecognition.continuous = true; // Permite reconhecimento de fala contínuo, até pedir para parar
     speechRecognition.maxAlternatives = 0; // Número máximo de alternativas de reconhecimento de fala
-    speechRecognition.interimResults = true; // Permite resultados no momento da fala e não apenas quando termina
+    speechRecognition.interimResults = false; // Permite resultados no momento da fala e não apenas quando termina
 
     speechRecognition.onresult = (e) => {
       // Converte o results para um array com métodos do array
